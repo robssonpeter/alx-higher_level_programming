e4d5aa843ee5ded55f97fdd1129b8d070f1c2918
Peter Mgembe <peterrobsson@gmail.com>
2023-03-07 11:27:43 -0800

    fixing issues

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for x in range(122, 96, -1):
-    if( x%2 == 0):
-        print(chr(x), end="")
+    if (x % 2 == 0):
+        print("{char}".format(char=chr(x)), end="")
     else:
-        print(chr(x-32), end="")
+        print("{char}".format(char=chr(x-32)), end="")
