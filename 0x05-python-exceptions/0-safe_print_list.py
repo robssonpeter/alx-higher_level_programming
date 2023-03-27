99c5e3f93411be934cfdf9c994be697194a80ee4
Peter Mgembe <peterrobsson@gmail.com>
2023-03-27 06:44:46 -0700

    minor correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         print("")
     except TypeError:
-        print("Please enter a valid type")
+        print("")
     else:
         print("")
     return returns
