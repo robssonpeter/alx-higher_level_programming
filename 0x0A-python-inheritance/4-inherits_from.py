ccd629d223470cc9a9e15140c4e3ace89624a24b
Peter Mgembe <peterrobsson@gmail.com>
2023-04-11 13:47:34 -0700

    solved 4 and 5

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" defines the function to check if inherits from class """
+
 
 def inherits_from(obj, a_class):
     """ checks if object inherits a class """
-    
+
     is_self = type(obj) == a_class
     return issubclass(type(obj), a_class) and not is_self
