c8c7681118bca22e305efdb61b7bcbe6b3ecb7ee
Peter Mgembe <peterrobsson@gmail.com>
2023-04-11 13:26:31 -0700

    solved question 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """ My List Class """
+    """ implementation of sorted list of items """
 
     def print_sorted(self):
         """ The function that prints the sorted list of items """
 
         the_list = sorted(self)
+
+        """ prints the sorted list."""
         print(the_list)
