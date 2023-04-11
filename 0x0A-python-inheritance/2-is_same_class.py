68f3bf1e2ed8104cbd09c4dd75432880a388387b
Peter Mgembe <peterrobsson@gmail.com>
2023-04-11 13:39:49 -0700

    solved question 3

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ defines the function to compare check the class """
 
+
 def is_same_class(obj, a_class):
     """ The function that checks if classes are same """
     return type(obj) == a_class
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" Define the is of class """
+
 
 def is_kind_of_class(obj, a_class):
     """ checking if an object is an instance of a class """
 
-    return type(obj) == a_class
+    return isinstance(obj, a_class)
