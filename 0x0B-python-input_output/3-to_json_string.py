df1124785fa435c5b1ed34f4607c4f97e4025b4d
Peter Mgembe <peterrobsson@gmail.com>
2023-04-12 07:57:36 -0700

    stil resolving 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 import json
+from io import StringIO
+
 """ The function for encoding to json """
 
 
 def to_json_string(my_obj):
     """ encode the string """
 
-    return json.dumps(my_obj)
+    io = StringIO()
+
+    json.dump(my_obj, io)
+
+    return io.getvalue()
