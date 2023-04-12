125d38ab969243ff37f3308e928a422fee2d5681
Peter Mgembe <peterrobsson@gmail.com>
2023-04-12 09:56:09 -0700

    solved question 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" the function to create a json stting and write to a file """
+import json
+
+
+json = __import__("json")
+def save_to_json_file(my_obj, filename):
+    """ the function to save to json file """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
