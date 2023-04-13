a757b003fabacae9b71ec916bdd5b18fd01cfbff
Peter Mgembe <peterrobsson@gmail.com>
2023-04-13 07:04:59 -0700

    fixed 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """ Defines the function to read agruments and add to list """
+
+
 from sys import argv
 import json
 
+""" import save data to a file """
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-prev = load_from_json_file("add_item.json")
-if len(prev) == 0:
-    prev = []
-args = argv[1:]
 
-new = prev + args
+if __name__ == "__main__":
+    prev = load_from_json_file("add_item.json")
+    if len(prev) == 0:
+        prev = []
+    args = argv[1:]
 
-save_to_json_file(new, "add_item.json")
+    new = prev + args
 
-# load_from_json_file("add_item.json")
+    save_to_json_file(new, "add_item.json")
