b60b35a01324a58edf3781a97b97224121094639
Peter Mgembe <peterrobsson@gmail.com>
2023-04-13 10:22:28 -0700

    solved q9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,7 +6,13 @@ class Student:
     """ the constructor """
 
     def __init__(self, first_name, last_name, age):
-        """ initializting the variables """
+        """ initializting the variables
+
+        Args:
+            first_name (str): the first name
+            last_name (str): the last name
+            age (int): the age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
