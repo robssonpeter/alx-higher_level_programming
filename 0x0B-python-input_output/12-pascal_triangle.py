9bb6785c405d1413ebca4695ca08023beb2ad86f
Peter Mgembe <peterrobsson@gmail.com>
2023-04-14 11:18:07 -0700

    solved q12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" defines a function to print pascal triangle """
+
+
+def pascal_triangle(n):
+    """ loop through n """
+
+    lis = []
+    for row in range(n):
+        if row == 0:
+            ls_row = [1]
+        else:    
+            ls_row = []
+        for el in range(row):
+            if el == 0:
+                ls_row.append(1)
+            if el > 0 and el < row:
+                ls_row.append(lis[row-1][el]+lis[row-1][el-1])
+            if el == row - 1:
+                ls_row.append(1)
+
+        lis.append(ls_row)
+    return lis
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -13,11 +13,13 @@ class Student:
             last_name (str): the last name
             age (int): the age
         """
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
         """ the function that converts the class instanc to json """
+
         class_to_json = __import__("8-class_to_json").class_to_json
         return class_to_json(self)
