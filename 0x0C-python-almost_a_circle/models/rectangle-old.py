14fb234557519457aad57e3cac394b83964088e3
Peter Mgembe <peterrobsson@gmail.com>
2023-04-17 10:19:41 -0700

    almost cycle project

---
+++ 0x0C-python-almost_a_circle/models/rectangle-old.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+""" The definition of a rectangle class """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Class rectangle with its attributes """
+
+    __width = 0
+    __height = 0
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = x
+        super().__init__(id)
+
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        if not isinstance(x, int):
+            raise TypeError('x must be an integer')
+        elif x < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Return the area of the Rectangle."""
+        return self.width * self.height
