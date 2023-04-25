c085c47fa517250c72b375d3b0b9c209f573032a
Peter Mgembe <peterrobsson@gmail.com>
2023-04-24 23:29:02 -0700

    Additional solutions

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""module for add ing iteger
+"""
+
 
 def add_integer(a, b=98):
+    """function add_integer"""
     allowed_types = [type(1), type(2.1)]
     if type(a) not in allowed_types:
         raise TypeError('a must be an integer')
