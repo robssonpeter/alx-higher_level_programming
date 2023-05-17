5b2be30dcbd98343d3c3cf8e92b8e19e3c4444bd
Peter Mgembe <peterrobsson@gmail.com>
2023-05-17 10:42:48 -0700

    solved 1 and 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,4 @@
 -- Creates a database hbtn and user user_od_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'locahost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
