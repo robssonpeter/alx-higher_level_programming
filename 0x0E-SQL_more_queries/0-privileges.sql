739f809a1866cc193f9d3c01527d325efea086f6
Peter Mgembe <peterrobsson@gmail.com>
2023-05-17 08:19:51 -0700

    solved the zeroth

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Privilieges checker
+-- Shows all privileges held by the user
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+Myslq More quieries assignment
