74f18edeb810ebdf78d43cc2bb4fe72960d55cb6
Peter Mgembe <peterrobsson@gmail.com>
2023-05-17 20:02:17 -0700

    solved 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- List shows
+-- by genres
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres 
+ON tv_show_genres.show_id = tv_shows.id
+WHERE genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
