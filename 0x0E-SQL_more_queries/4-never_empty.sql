2269b269afe588edd1eab259ff57a8d315830c08
Peter Mgembe <peterrobsson@gmail.com>
2023-05-17 18:51:23 -0700

    solved to 5

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- Create a table
+-- create a table with a non nullable id column
+CREATE TABLE id_not_null IF NOT EXISTS (id INT NOT NULL, name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,3 @@
+-- Table unique id
+-- Create table with unique id
+CREATE TABLE unique_id IF NOT EXISTS (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
