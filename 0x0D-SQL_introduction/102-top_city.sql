28f5aa3ed45e488973ac273b552e1f61b8e32f25
Peter Mgembe <peterrobsson@gmail.com>
2023-05-18 08:10:17 -0700

    solved 102

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,4 @@
 -- Average temp
 -- Based on cities
-SELECT city, AVG(value) AS avg_temp FROM temperatures
-WHERE month >= 7 AND month <= 8
-GROUP BY city ORDER BY AVG(avg_temp) DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month >=
+7 AND month <= 8 GROUP BY city ORDER BY AVG(value) DESC LIMIT 3;
