e8d80ec5876f515b98d03e1906f3ae0bdc659142
Peter Mgembe <peterrobsson@gmail.com>
2023-05-18 11:03:48 -0700

    solved question 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- List items
 -- List items of the states table
 SELECT * FROM TABLE cities
-WHERE state_id = (SELECT id FROM states)
-ORDER BY id ASC;
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;
