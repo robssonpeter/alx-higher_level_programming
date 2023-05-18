2cc2a518c0e69fda9dab3e63282e46e09a394067
Peter Mgembe <peterrobsson@gmail.com>
2023-05-18 11:23:20 -0700

    solved question 16

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- List shows
 -- by genres
-SELECT tv_shows.title FROM tv_genres 
-LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id 
-INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id 
-WHERE tv_genres.name = 'Comedy' ORDER BY tv_shows.title ASC;
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC;
