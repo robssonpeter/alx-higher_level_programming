64b5439614acba26890f2d95cc7de4913f74115a
Peter Mgembe <peterrobsson@gmail.com>
2023-06-16 05:31:41 -0700

    trying to solve sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     args = sys.argv
     connection = MySQLdb.connect("localhost", args[1], args[2], args[3])
     cursor = connection.cursor()
-    stmnt = "SELECT id, name FROM states WHERE name = %name ORDER BY id ASC"
-    cursor.execute(stmnt, args[4])
+    stmnt = "SELECT id, name FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(stmnt, (args[4],))
     results = cursor.fetchall()
     for result in results:
         print(result)
