62019071a6b445d24388b3b82cdba1cdb5e5e411
Peter Mgembe <peterrobsson@gmail.com>
2023-06-17 15:05:24 +0300

    trying to solve 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-class State:
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
