ee656f3d4c6503aff1d9a7609f8d36fcd3ea97a1
Peter Mgembe <peterrobsson@gmail.com>
2023-07-01 02:47:27 -0700

    solving question zero

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,4 +9,4 @@ with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
 print("Body response:")
 print(f"\t- type: {type(data)}")
 print(f"\t- content: {data}")
-print(f"\t- utf8 content {data.decode('utf-8')}")
+print(f"\t- utf8 content: {data.decode('utf-8')}")
