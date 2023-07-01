b74c0cb62e910113d96594577cbb46c8ce4cbf92
Peter Mgembe <peterrobsson@gmail.com>
2023-07-01 02:45:29 -0700

    solved question zero

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ from urllib import request
 
 with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     data = response.read()
-print("Body response:$")
-print(f"\t- type: {type(data)}$")
-print(f"\t- content: {data}$")
-print(f"\t- utf8 content {data.decode('utf-8')}$")
+print("Body response:")
+print(f"\t- type: {type(data)}")
+print(f"\t- content: {data}")
+print(f"\t- utf8 content {data.decode('utf-8')}")
