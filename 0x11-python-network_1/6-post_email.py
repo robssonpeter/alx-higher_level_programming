24a2ffd483e5423f56835d1f938aa7c73eb3a276
Peter Mgembe <peterrobsson@gmail.com>
2023-07-02 02:39:19 -0700

    solveed question 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     email = sys.argv[2]
     url = sys.argv[1]
     params = {"email": email}
-    with requests.post(url, params=params) as resp:
+    with requests.post(url, data=params) as resp:
         print(resp.text)
