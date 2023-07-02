d972b5d170bda1d61bac9ad61314baedbea94106
Peter Mgembe <peterrobsson@gmail.com>
2023-07-01 23:58:31 -0700

    solved question 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Make a pos request passing an email in params """
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    email = sys.argv[2]
+    url = sys.argv[1]
+    params = {"email":email}
+    with requests.post(url, params=params) as resp:
+        print(resp.text())
