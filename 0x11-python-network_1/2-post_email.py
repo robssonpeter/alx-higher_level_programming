c26ae507f6f2d2a6bbc40d00ad10ead85df7a765
Peter Mgembe <peterrobsson@gmail.com>
2023-07-02 03:42:32 -0700

    solved question 3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,13 +2,15 @@
 """ the script to take an email and pass to
 parameters """
 
+
 from urllib.request import Request
+import urllib.request
 import urllib.parse
 from sys import argv
 
 if (len(argv) > 2):
     obj = {"email": argv[2]}
-    data = urllib.parse.urlencode(obj).encode()
+    data = urllib.parse.urlencode(obj).encode('ascii')
     request = Request(argv[1], data, method="POST")
-    with request as response:
-        print(response.read().decode('utf-8'))
+    with urllib.request.urlopen(request) as resp:
+        print(resp.read().decode('utf-8'))
