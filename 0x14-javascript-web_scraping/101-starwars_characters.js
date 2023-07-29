1e6bc153142539b3b85155925e4ae37a99962d0d
Peter Mgembe <peterrobsson@gmail.com>
2023-07-29 21:17:12 +0300

    solved the advanced question 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,22 +2,33 @@
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-// console.log(url)
-// fetch the movie
 request.get(url, (error, response) => {
   if (!error) {
     const characters = JSON.parse(response.body).characters;
+    const charIds = [];
+    characters.sort();
+    let currentChar = [];
     for (let x = 0; x < characters.length; x++) {
-      request.get(characters[x], (err, resp) => {
+      currentChar = characters[x].split('/');
+      charIds.push(Number(currentChar[currentChar.length - 2]));
+    }
+    charIds.sort((a, b) => a - b);
+    const url = 'https://swapi-api.alx-tools.com/api/people/';
+    const output = {};
+    for (let x = 0; x < charIds.length; x++) {
+      request.get(url + charIds[x], (err, resp) => {
         if (!err) {
           const body = JSON.parse(resp.body);
-          console.log(body.name);
+          output[x] = body.name;
         }
       });
     }
+    setTimeout(() => {
+      for (let x = 0; x < charIds.length; x++) {
+        console.log(output[x]);
+      }
+    }, 5000);
   } else {
     console.log(error);
   }
 });
-// get the characters
-// loop through each character and get their name
